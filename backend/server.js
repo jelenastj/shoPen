3af8a214a9f5d8e413420774bf00968939d58956
jelenastj <jelena.stjepanovic@gmail.com>
2020-10-25 20:14:13 -0700

    colors added

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv  from 'dotenv'
 import connectDB from './config/db.js'
 import products from './data/products.js'
+import colors from 'colors'
 
 const app = express()
 dotenv.config()
@@ -23,4 +24,4 @@ app.get('/api/products/:id', (req, res) => {
 const PORT = process.env.PORT || 5000
 app.listen(
   PORT, 
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
