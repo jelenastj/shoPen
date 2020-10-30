a932fe620c30900df4fba447acb463ab4559482a
jelenastj <jelena.stjepanovic@gmail.com>
2020-10-30 13:05:41 -0700

    user auth endpoint

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,7 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
+
+
 const userSchema = mongoose.Schema({
   name: {
   type: String,
@@ -21,6 +24,11 @@ const userSchema = mongoose.Schema({
       }, { 
          timestamp: true
 })    
+
+userSchema.methods.matchPassword = async function(enteredPassword) {
+return await bcrypt.compare(enteredPassword, this.password)
+}
+
 
 const User = mongoose.model('User', userSchema)
 
