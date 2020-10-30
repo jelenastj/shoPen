994e44974ff678af5334758a34cd4b6d97bdd4bd
jelenastj <jelena.stjepanovic@gmail.com>
2020-10-29 18:58:50 -0700

    remove item

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import {
-  CART_ADD_ITEM
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM
 } from '../constants/cartConstants'
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
@@ -20,3 +21,11 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
 
   localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
 }
+
+export const removeFromCart = (id) => (dispatch, getState) => {
+  dispatch({
+    type: CART_REMOVE_ITEM,
+    payload: id
+  })
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+} 
