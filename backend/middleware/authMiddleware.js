7cd268891925318742606b326f8abc16c35462a7
jelenastj <jelena.stjepanovic@gmail.com>
2020-11-01 09:19:57 -0800

    user registration and password encryption

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -10,9 +10,11 @@ const protect = asyncHandler(async (req, res, next) => {
       req.headers.authorization.startsWith('Bearer')
   ) {
       try {
-          token = req.headers.authorization.split(' ')[1]
-          const decoded = jwt.verify(token, process.env.JWT_SECRET)
-          req.user = await User.findById(decoded.id).select('-password')
+        token = req.headers.authorization.split(' ')[1]
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        req.user = await User.findById(decoded.id).select('-password')
           next()
       } catch (error) {
           console.error(error)
